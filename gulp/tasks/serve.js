f158e8fbc80735137d428309e1a121bb968ccdb0
Ivan Bochkarev <ivanx86@gmail.com>
2019-07-01 17:18:47 +0600

    Minor changes

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -4,8 +4,8 @@ module.exports = function () {
       server: './build',
       open: true,
       // online: false, // Work Offline Without Internet Connection
-      tunnel: true,
-      tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+      // tunnel: true,
+      // tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
     });
   });
 };
